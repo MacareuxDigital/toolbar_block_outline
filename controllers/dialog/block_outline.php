82d8c7648a0455c31220657f851c036033591fe1
Takuro Hishikawa <hishikawa@concrete5.co.jp>
2022-02-16 07:11:34 +0900

    Display custom template, cache lifetime, alias of master collection

--- controllers/dialog/block_outline.php
+++ controllers/dialog/block_outline.php
@@ -4,6 +4,7 @@ namespace Concrete\Package\ToolbarBlockOutline\Controller\Dialog;
 
 use Concrete\Controller\Backend\UserInterface\Page;
 use Concrete\Core\Page\Stack\Stack;
+use Concrete\Core\Url\Resolver\Manager\ResolverManagerInterface;
 
 class BlockOutline extends Page
 {
@@ -11,17 +12,22 @@ class BlockOutline extends Page
 
     public function view()
     {
+        /** @var ResolverManagerInterface $resolver */
+        $resolver = $this->app->make(ResolverManagerInterface::class);
         /** @var \Concrete\Core\Page\Page $page */
         $page = $this->page;
         $areaBlocks = [];
         $blocks = $page->getBlocks();
         foreach ($blocks as $block) {
-            $areaBlocks[$block->getAreaHandle()][] = $block;
+            $areaBlocks[$block->getAreaHandle()]['blocks'][] = $block;
         }
         $globalBlocks = $page->getGlobalBlocks();
         foreach ($globalBlocks as $block) {
-            $stack = Stack::getByID($block->getBlockCollectionID());
-            $areaBlocks[t('Sitewide %s', $stack->getStackName())][] = $block;
+            $stackID = $block->getBlockCollectionID();
+            $stack = Stack::getByID($stackID);
+            $stackName = t('Sitewide %s', $stack->getStackName());
+            $areaBlocks[$stackName]['link'] = $resolver->resolve(['/dashboard/blocks/stacks', 'view_details', $stackID]);
+            $areaBlocks[$stackName]['blocks'][] = $block;
         }
         $this->set('areaBlocks', $areaBlocks);
         $this->set('isEditMode', $page->isEditMode());
